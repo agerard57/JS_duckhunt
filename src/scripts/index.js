33f3350d8d0c514ea9b0084bf8bff9d818917681
GERARD Alexandre <agerard57@protonmail.com>
2022-02-28 22:12:46 +0100

    refacto: Fix indenting

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
 const scripts = ["countdown", "duck"];
 
 scripts.forEach((element) =>
-  document.writeln(
-    '<script type="text/javascript" src="src/scripts/' +
-      element +
-      '.js"></script>'
-  )
+    document.writeln(
+        '<script type="text/javascript" src="src/scripts/' +
+        element +
+        '.js"></script>'
+    )
 );
