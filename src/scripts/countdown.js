3e9020b7e751ffd41c7182720ce111272be597e2
GERARD Alexandre <agerard57@protonmail.com>
2022-02-28 22:12:27 +0100

    refacto: Fix indenting

--- src/scripts/countdown.js
+++ src/scripts/countdown.js
@@ -1,20 +1,20 @@
 const hurryValue = 10;
 var timeLeft = 60;
 
-const countdown = setInterval(function () {
-  var counter = document.querySelector("#countdown-counter");
-  counter.style.color = "#06e515";
-  counter.innerHTML = timeLeft--;
+const countdown = setInterval(function() {
+    var counter = document.querySelector("#countdown-counter");
+    counter.style.color = "#06e515";
+    counter.innerHTML = timeLeft--;
 
-  if (timeLeft < 10) {
-    counter.style.color = "orange";
-  }
-  if (timeLeft < 5) {
-    counter.style.color = "red";
-  }
-  if (timeLeft < 0) {
-    clearInterval(countdown);
-    counter.innerHTML = "END";
-    counter.style.color = "red";
-  }
+    if (timeLeft < 10) {
+        counter.style.color = "orange";
+    }
+    if (timeLeft < 5) {
+        counter.style.color = "red";
+    }
+    if (timeLeft < 0) {
+        clearInterval(countdown);
+        counter.innerHTML = "END";
+        counter.style.color = "red";
+    }
 }, 1000);
